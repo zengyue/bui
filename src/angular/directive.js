1cdf060009c788661a28e4c9d6ce577b60a0c8be
索丘 <zengyue.yezy@alibaba-inc.com>
2014-05-12 15:02:59 +0800

    angular

--- src/angular/directive.js
+++ src/angular/directive.js
@@ -2,31 +2,57 @@
  * BUI对angular的封装
  */
 define('bui/angular/directive', function(require){
-  
+
+  function loadPath(path){
+    return path.toLowerCase();
+  }
+
 
   angular.module("bui.angular.directive", [])
 
-  //定义form指令
-  .directive('form', [function($rootScope, $q){
+  .factory('controllerFactory', ['$q', function($q){
     return {
-      restrict: 'AE',
-      link: function(scope, element, attrs){
-        BUI.use('bui/form', function(Form){
-          var form = new Form.HForm({
-            srcNode: element,
-            autoRender: true
-          });
+      create: function(namespace){
+        var deferred = $q.defer();
+
+        var space = namespace.split('.');
+
+        BUI.use(loadPath(space[0]), function(Controller){
+          deferred.resolve(Controller[space[1]])
         });
+
+        return deferred.promise;
       }
     }
   }])
+
   //定义
-  .directive('buiSelect', [function(){
+  .directive('buiRole', ['controllerFactory', function(controllerFactory){
     return {
       restrict: 'AE',
+      scope: {},
       link: function(scope, element, attrs){
-        BUI.use('bui/select', function(Select){
+        var namespace = attrs['buiRole'],
+          $parent = scope.$parent,
+          id = attrs.id;
+
+        if(id){
+          if($parent[id]){
+            BUI.mix(scope, $parent[id]);
+          }
+          $parent[id] = scope;
+        }
+
+        controllerFactory.create(namespace)
+        .then(function(Controller){
+          new Controller({
+            render: element,
+            autoRender: true,
+            items: scope.items
+          });
         });
+
+
       }
     }
   }]);
--- test/angular/angular.php
+++ test/angular/angular.php
@@ -1,27 +1,37 @@
 <!doctype html>
-<html lang="en">
+<html ng-app="app">
 <head>
   <meta charset="utf-8">
   <title>Angular 测试</title>
-  <link href="http://cdn.bootcss.com/bootstrap/3.1.1/css/bootstrap.css" rel="stylesheet">
   <script src="http://g.tbcdn.cn/fi/bui/jquery-1.8.1.min.js"></script>
   <script src="http://g.tbcdn.cn/fi/bui/seed.js"></script>
   <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular.js"></script>
   <script src="../../src/angular/base.js"></script>
   <script src="../../src/angular/directive.js"></script>
+
+  <link rel="stylesheet" href="/git/bui/assets/css/bs3/dpl.css">
+  <link rel="stylesheet" href="/git/bui/assets/css/bs3/bui.css">
 </head>
 <body>
-  <form>
-    
-  </form>
-
-  <bui-select>
-  </bui-select>
+  
+  <div ng-controller="MyCtrl">
+    <div id="a" bui-role="Select.Select" ></div>
+  </div>
 
   <script>
   BUI.use(['bui/angular/base', 'bui/angular/directive'], function(){
-    console.log(111);
-    angular.bootstrap(document, ['bui.angular.directive']);
+    
+    angular.module('app', ['bui.angular.directive'])
+    .controller('MyCtrl', ['$scope', function($scope){
+      $scope.a = {
+        items: [
+          {text:'选项1',value:'a'},
+          {text:'选项2',value:'b'},
+          {text:'选项3',value:'c'}
+        ]
+      }
+    }]);
+    
   });
   </script>
 </body>
